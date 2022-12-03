ac8829b8b28be98ca2d4919d576746b32c55f3d4
KrysFreeman <ksfreeman13@gmail.com>
2022-12-03 14:32:37 -0700

    word done

--- index.js
+++ index.js
@@ -4,22 +4,20 @@
 // variables
 const keys = document.querySelectorAll(".keys");
 let answer = "";
+let guessed = [];
+let wordStatus = null;
+
 
 // word array
 let word = ["wreck", "history", "apple", "walrus"];
 
-// new game button
-
-// image change at wrong guess
-function imageChange() {}
 
 // word randomizer
-function wordPick() {
+function randomWord() {
   answer = word[Math.floor(Math.random() * word.length)];
-
 }
 
-wordPick();
+randomWord()
 
 // keyboard buttons opacity
 addEventListener("click", ( event ) => {
@@ -28,4 +26,12 @@ addEventListener("click", ( event ) => {
     selectedButton.style.opacity = '.5'
 });
 
+// word to guess
+function guessWord () {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('')
+    document.getElementById('wordSpotlight').innerHTML = wordStatus
+}
 
+guessWord()
+
+// new game button
